53d8814fa70fd398d83d1c5e6777f4d62a1358e3
RiviereGregory <gregory.riviere@laposte.net>
2021-07-05 22:03:08 +0200

    [Pokemons] utilisation de 3 algo pour savoir le quel est le mieux puis sauvegarde du modele d'apprenstissage

--- Pokemons/apprentisage.py
+++ Pokemons/apprentisage.py
@@ -2,8 +2,13 @@
 # IMPORT DES MODULES
 # ***********************
 
+import joblib
 import pandas as pnd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
 # ***********************
 # APPRENTISSAGE
@@ -24,3 +29,82 @@ y = dataset.iloc[:, 17].values
 
 # Construction du jeu d'entrainement et du jeu de test
 X_APPRENTISSAGE, X_VALIDATION, Y_APPRENTISSAGE, Y_VALIDATION = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# ***************************************
+# ALGORITHME 1: REGRESSION LINEAIRE    *
+# *************************************
+
+# Choix de l'algorithme
+algorithme = LinearRegression()
+
+# Apprentissage à l'aide de la fonction fit
+algorithme.fit(X_APPRENTISSAGE, Y_APPRENTISSAGE)
+
+# Realisation de la prédiction sur le jeu  de test
+predictions = algorithme.predict(X_VALIDATION)
+
+# Calcul de la précision de l'apprentissage à l'aide de la
+# fonction r2_score
+precision = r2_score(Y_VALIDATION, predictions)
+
+print(">> ----------- REGRESSION LINEAIRE -----------")
+print(">> Precision = " + str(precision))
+print("------------------------------------------")
+
+#  REGRESSION LINEAIRE
+# >> Precision = 0.9043488485570965
+
+# ************************************************************
+# ALGORITHME 2: ARBRE DE DECISION APPLIQUE A LA REGRESSION  *
+# **********************************************************
+
+# Choix de l'algorithme
+algorithme = DecisionTreeRegressor()
+
+# Apprentissage à l'aide de la fonction fit
+algorithme.fit(X_APPRENTISSAGE, Y_APPRENTISSAGE)
+
+# Realisation de la prédiction sur le jeu  de test
+predictions = algorithme.predict(X_VALIDATION)
+
+# Calcul de la précision de l'apprentissage à l'aide de la
+# fonction r2_score
+precision = r2_score(Y_VALIDATION, predictions)
+
+print(">> ----------- ARBRES DE DECISION -----------")
+print(">> Precision = " + str(precision))
+print("------------------------------------------")
+
+# ARBRES DE DECISION
+# >> Precision = 0.8684246994972593
+
+# *********************************************************
+# ALGORITHME 3: RANDOM FOREST APPLIQUE A LA REGRESSION  *
+# ******************************************************
+
+# Choix de l'algorithme
+algorithme = RandomForestRegressor()
+
+# Apprentissage à l'aide de la fonction fit
+algorithme.fit(X_APPRENTISSAGE, Y_APPRENTISSAGE)
+
+# Realisation de la prédiction sur le jeu  de test
+predictions = algorithme.predict(X_VALIDATION)
+
+# Calcul de la précision de l'apprentissage à l'aide de la
+# fonction r2_score
+precision = r2_score(Y_VALIDATION, predictions)
+
+print(">> ----------- RANDOM FOREST -----------")
+print(">> Precision = " + str(precision))
+print("------------------------------------------")
+
+# RANDOM FOREST
+# >> Precision = 0.9376935860217895
+
+
+# Résultat ici c'est le random forest qui a le meilleur score
+
+# Sauvegarde de l'algorithme
+fichier = 'modele/modele_pokemon.mod'
+joblib.dump(algorithme, fichier)

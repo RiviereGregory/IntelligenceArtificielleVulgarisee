4a06b1f5a8a677a02c0c4f5505cc2096703606f8
RiviereGregory <gregory.riviere@laposte.net>
2021-07-25 20:41:06 +0200

    [CALSSIFICATION_TEXTES] init projet + import données et normalisation

---
+++ classificationTexte/main.py
@@ -0,0 +1,27 @@
+import re
+
+import pandas as pnd
+
+messagesTwitter = pnd.read_csv("datas/rechauffementClimatique.csv", ";")
+
+print(messagesTwitter.shape)
+print(messagesTwitter.head(2))
+
+messagesTwitter['CROYANCE'] = (messagesTwitter['CROYANCE'] == 'Yes').astype(int)
+print(messagesTwitter.head(100))
+
+
+################################
+# ------ NORMALISATION ------ #
+#############################
+def normalisation(message):
+    message = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', message)
+    message = re.sub('@[^\s]+', 'USER', message)
+    message = message.lower().replace("ё", "е")
+    message = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', message)
+    message = re.sub(' +', ' ', message)
+    return message.strip()
+
+
+messagesTwitter["TWEET"] = messagesTwitter["TWEET"].apply(normalisation)
+print(messagesTwitter.head(10))

3c8925e02e1dc545062705008c00f0e99daa08cf
RiviereGregory <gregory.riviere@laposte.net>
2021-10-08 21:51:17 +0200

    [LectureLettre] reconnaisance lettre et lecture a haute voix

--- lectureLettre/lecture.py
+++ lectureLettre/lecture.py
@@ -1,11 +1,60 @@
+# Module de gestion des processus
+import threading
+
 import cv2
 import numpy as np
+# Module de paroles
+import pyttsx3 as pyttsx
+# Module Keras permettant l'utilisation de notre réseau de neurones
+from tensorflow.keras.models import load_model
 
 # dimensions de l'ardoise
 zoneEcritureLongueurMin = 300
 zoneEcritureLongueurMax = 500
 zoneEcritureLargeurMin = 200
 zoneEcritureLargeurMax = 400
+# Par défaut on active la lecture de lettre à voix haute
+lectureActivee = True
+
+# Temps d'attente en secondes entre chaque lecture de lettre à voix haute
+dureeDesactivationLectureDeLettre = 5
+
+# Initialisation de la voix
+print('Initialisation de la voix')
+engine = pyttsx.init()
+
+# Choix de la voix en français
+voice = engine.getProperty('voices')[0]
+engine.setProperty('voice', voice.id)
+
+# test de la voix
+engine.say('Mode lecture de lettres activé')
+engine.runAndWait()
+
+
+# fonction de reactivation de la lecture de lettre à voix haute
+def activation_lecture():
+    print('Activation de la lecture de lettres')
+    global lectureActivee
+    lectureActivee = True
+
+
+print('Initialisation du modèle d''aprentissage')
+# Chargement du modèle entrainé
+cnn_model = load_model('modele/modele_cas_pratiqueV3.h5')
+kernel = np.ones((5, 5), np.uint8)
+
+# Tableau de lettres avec leur numéro
+lettres = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J',
+           11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T',
+           21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z', 27: '-'}
+# Par défaut on choisi que la lettre Z est détectée
+prediction = 26
+
+# Par défaut aucune détection n'est faite.
+lettrePredite = False
+
+isContoursLettre = False
 
 print('Initialisation de la webcam')
 webCam = cv2.VideoCapture(0)
@@ -17,6 +66,9 @@ else:
     print('ERREUR')
 
 while True:
+
+    # Par défaut aucune détection n'est faite.
+    lettrePredite = False
 
     # Capture de l'image dans la variable Frame
     # La variable lectureOK est égale à True si la fonction read() est opérationnelle
@@ -85,14 +137,52 @@ while True:
 
                         # Si ce n'est pas une ombre, on affiche la lettre capturée à l'écran
                         if affichageLettreCapturee:
-                            # cv2.destroyWindow("ContoursLettre")
+                            if isContoursLettre:
+                                cv2.destroyWindow("ContoursLettre")
                             cv2.imshow("ContoursLettre", captureLettre)
+                            isContoursLettre = True
 
                             # Redimensionnement de l'image
                             newImage = cv2.resize(captureLettre, (28, 28))
                             newImage = np.array(newImage)
                             newImage = newImage.astype('float32') / 255
                             newImage.reshape(1, 28, 28, 1)
+
+                            # Realisation de la prediction
+                            prediction = cnn_model.predict(newImage.reshape(1, 28, 28, 1))[0]
+                            prediction = np.argmax(prediction)
+
+                            # On indique qu'une lettre a été détectée
+                            lettrePredite = True
+
+                if lettrePredite:
+
+                    # On desactive la lecture de lettre à voix haute
+                    print(
+                        'Desactivation de la lecture de lettre ' + str(dureeDesactivationLectureDeLettre) + " secondes")
+                    lectureActivee = False
+
+                    # On affiche le numéro de la lettre prédit
+                    # On ajoute +1 car la première lettre de l'alphabet a pour valeur 0 dans notre modèle de prédiction
+                    # Alors qu'elle a la valeur 1 dans notre tableau de correspondance
+                    print("Detection:" + str(lettrePredite))
+                    print("Prediction = " + str(prediction))
+
+                    # Lecture à voix haute de la lettre prédit
+                    if lettrePredite and prediction != 26:
+                        engine.say('Je lis la lettre ' + str(lettres[int(prediction) + 1]))
+                        engine.runAndWait()
+                        lettrePredite = False
+
+                    if lettrePredite and prediction == 26:
+                        engine.say('Je ne comprends pas la lettre écrite ')
+                        engine.runAndWait()
+                        lettrePredite = False
+
+                    # Pause du processus de lecture de la lettre puis appel à la fonction pour la reactivation de la
+                    # lecture
+                    timer = threading.Timer(dureeDesactivationLectureDeLettre, activation_lecture)
+                    timer.start()
 
     # Affichage de l'image capturée par la webcam
     cv2.imshow("IMAGE", frame)
